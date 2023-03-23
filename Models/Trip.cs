ed4a281bf688098adab4d3b2ef90f87d0bd70a0a
prudvi-m <prudhvi.manukonda4@google.com>
2023-03-22 19:30:45 -0500

    destination accommodation validation

--- Models/Trip.cs
+++ Models/Trip.cs
@@ -7,16 +7,22 @@ namespace TripLog.Models
     {
         public int TripID { get; set; }
 
-        [Required(ErrorMessage = "Please enter a destination")]
+        [Required(ErrorMessage = "Please enter a Destination")]
+        [RegularExpression(@"^[a-zA-Z''-'\s]{1,30}$", 
+        ErrorMessage = "Special characters & numbers are not allowed & only 30 characters length were accepted.")]
         public string Destination { get; set; }
 
-        [Required(ErrorMessage = "Please enter the date your trip starts.")]
+        // [Required(ErrorMessage = "Please ÷enter the date your trip starts.")]
         public DateTime? StartDate { get; set; }
 
-        [Required(ErrorMessage = "Please enter the date your trip ends.")]
+        // [Required(ErrorMessage = "Please enter the date your trip ends.")]
         public DateTime? EndDate { get; set; }
 
+        [Required(ErrorMessage = "Please enter a Accommodation")]
+        [RegularExpression(@"^[a-zA-Z1-9''-'\s]{1,50}$", 
+        ErrorMessage = "Special characters are not allowed & only 50 characters length were accepted.")]
         public string Accommodation { get; set; }
+
         public string AccommodationPhone { get; set; }
         public string AccommodationEmail { get; set; }
 
